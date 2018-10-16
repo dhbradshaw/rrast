0410416a420fff1bd51164be7b2a3208170de66b
Douglas H. Bradshaw <dhbradshaw@gmail.com>
2018-10-16 12:21:11 -0500

    redis hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate redis;
+
+use redis::{Client, Commands};
+
 fn main() {
-    println!("Hello, world!");
+    let client = Client::open("redis://127.0.0.1/").unwrap();
+    let conn = client.get_connection().unwrap();
+    let _: () = conn.set("answer", 42i32).unwrap();
+    let answer: i32 = conn.get("answer").unwrap();
+    println!("Answer: {}", answer);
 }
